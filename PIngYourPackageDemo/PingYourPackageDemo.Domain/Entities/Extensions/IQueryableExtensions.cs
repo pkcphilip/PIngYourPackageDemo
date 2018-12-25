2f2f77da0f2f37f0a0b5c9445103e86fa82fbc63
Philip Pang <pkcphilip@outlook.com>
2018-12-25 15:23:08 +0800

    Create repository interface

---
+++ PIngYourPackageDemo/PingYourPackageDemo.Domain/Entities/Extensions/IQueryableExtensions.cs
@@ -0,0 +1,16 @@
+﻿using PingYourPackageDemo.Domain.Entities.Core;
+using System.Linq;
+
+namespace PingYourPackageDemo.Domain.Entities.Extensions
+{
+    public static class IQueryableExtensions
+    {
+        public static PaginatedList<T> ToPaginatedList<T>(this IQueryable<T> query, int pageIndex, int pageSize)
+        {
+            var totalCount = query.Count();
+            var collection = query.Skip((pageIndex - 1) * pageSize).Take(pageSize);
+
+            return new PaginatedList<T>(pageIndex, pageSize, totalCount, collection);
+        }
+    }
+}
